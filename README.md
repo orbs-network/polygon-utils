27c54ebd874556781414a84ec93312fdf7702f69
Degenddy <degenddy@users.noreply.github.com>
2022-04-07 18:16:35 +0300

    changed the disk size util to be more specific. added disk cleanup util

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This includes actions and utils to help maintain existing orbs-nodes.
 ### 1. Modify EBS (os) disk size
 This util should be used to increase/decrease the EBS disk size (without downtime)
 
-```polygon-cleanup all --region us-east-1 --profile default -q swarm-manager-0xMYNODE-profile```
+```polygon-cleanup disk-size```
--- bin/polygon-utils.js
+++ bin/polygon-utils.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
 const {program} = require('commander');
-const polygonUtils = require('../lib/disk-size');
+const IncreaseDiskSize = require('../lib/increase-disk-size');
+const CleanDisk = require('../lib/clean-disk');
 
 program
-    .command('disk-size --region --profile')
-    .description('Performs a EBS size change on aws and updates the linux FS')
-    .option('--profile <profileName>', 'The AWS profile to use')
-    .action(polygonUtils.start);
+    .command('increase-disk-size --profile')
+    .description('Increase EBS size from 8GB to 16GB on aws and updates the linux Partition and FileSystem')
+    .option('--profile <profileName>', 'The AWS profile to use (optional)', 'default')
+    .action(IncreaseDiskSize.flow_start);
+
+program
+    .command('clean-disk --profile')
+    .description('Executes several shell commands to clean unnecessary files from the os')
+    .option('--profile <profileName>', 'The AWS profile to use (optional)', 'default')
+    .action(CleanDisk.flow_start);
 
 program.parse(process.argv);
