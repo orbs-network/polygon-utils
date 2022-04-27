02e6ca6933e9333a24e8565f37a063281dac5aac
Degenddy <degenddy@users.noreply.github.com>
2022-04-27 12:35:20 +0300

    including ssh init on aws

--- bin/polygon-utils.js
+++ bin/polygon-utils.js
@@ -5,9 +5,11 @@ const IncreaseDiskSize = require('../lib/increase-disk-size');
 const CleanDisk = require('../lib/clean-disk');
 
 program
-    .command('increase-disk-size --profile')
+    .command('increase-disk-size --profile --automatic-partition-resize')
     .description('Increase EBS size from 8GB to 16GB on aws and updates the linux Partition and FileSystem')
     .option('--profile <profileName>', 'The AWS profile to use (optional)', 'default')
+    .option('--advanced-mode <advancedMode>', 'Run in advanced mode which prompt for more options', false)
+    .option('--automatic-partition-resize <automaticPartitionResize>', 'Attempt to automaticly resize of partition and file system (this uses ssh to connect to the instance)', false)
     .action(IncreaseDiskSize.flow_start);
 
 program
